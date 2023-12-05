27c1726297058270e4d7a880bb67a93faeb1f21e
Max Forsey <maxforsey@I-am-Wise.local>
2023-12-05 14:25:12 -0700

    built first react app with local storage following this tutorial https://blog.logrocket.com/notes-app-react-localstorage/#notes-grid

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { React } from "react";
+import './components/css/App.css';
+import Header from "./components/NoteComponents/Header"
+import Notes from "./components/NoteComponents/Notes"
+
+function App() {
+  return (
+    <div className="main">
+      <Header />
+      <Notes />
+    </div>
+  );
+}
+
+export default App;
